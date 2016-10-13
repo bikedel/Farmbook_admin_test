33c7b57f38d183feea0ac88e1155871ce1adb10c
Bike Del <home@Homes-iMac.Home>
2016-10-13 14:00:21 +0200

    err msg change

--- app/Http/Controllers/VuePropertyController.php
+++ app/Http/Controllers/VuePropertyController.php
@@ -205,7 +205,7 @@ class VuePropertyController extends Controller
         $properties = Property::on($database)->where('numErf', $numErf)->where('strIdentity', $identity)->get();
 
         if ($properties->count()) {
-            return response()->json(['test' => 'The Id is already associated with this Erf.'], 422);
+            return response()->json(['test' => 'The Id Number is already associated with this Erf.'], 422);
         }
 
         // add new record
@@ -334,7 +334,7 @@ class VuePropertyController extends Controller
         $properties = Property::on($database)->where('numErf', $numErf)->where('strIdentity', $identity)->where('id', '!=', $id)->get();
 
         if ($properties->count()) {
-            return response()->json(['test' => 'The Id is already associated with this Erf.'], 422);
+            return response()->json(['test' => 'The Id Number is already associated with this Erf.'], 422);
         }
 
         // remove id from the form request
