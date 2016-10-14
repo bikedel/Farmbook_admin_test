76a3afc38618440ad12f58c1abe6c881532d5598
Bike Del <home@Homes-iMac.Home>
2016-10-14 08:53:32 +0200

    owner phone numeric validation

--- app/Http/Controllers/VueOwnerController.php
+++ app/Http/Controllers/VueOwnerController.php
@@ -154,9 +154,9 @@ class VueOwnerController extends Controller
             'strIDNumber'    => 'required|unique:' . $database . '.owners',
             'strSurname'     => 'required',
             'strFirstName'   => 'required',
-            'strHomePhoneNo' => 'required',
-            'strWorkPhoneNo' => 'required',
-            'strCellPhoneNo' => 'required',
+            'strHomePhoneNo' => 'required|Numeric',
+            'strWorkPhoneNo' => 'required|Numeric',
+            'strCellPhoneNo' => 'required|Numeric',
             'EMAIL'          => 'required|email',
 
         );
@@ -169,6 +169,9 @@ class VueOwnerController extends Controller
             'strHomePhoneNo.required' => 'This field is required',
             'strWorkPhoneNo.required' => 'This field is required',
             'strCellPhoneNo.required' => 'This field is required',
+            'strHomePhoneNo.numeric'  => 'This field must be numeric',
+            'strWorkPhoneNo.numeric'  => 'This field must be numeric',
+            'strCellPhoneNo.numeric'  => 'This field must be numeric',
             'EMAIL.required'          => 'This field is required',
             'EMAIL.email'             => 'This field must be a valid email address',
         );
