0e2cc215172f91d16378b5aa2493fdcfaa23867c
Bike Del <home@Homes-iMac.Home>
2016-10-16 12:40:45 +0200

    order users by name

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -35,7 +35,7 @@ class UserController extends Controller
 
         // dd("user controller");
 
-        $users = User::all();
+        $users = User::all()->orderBy('Name');
 
         return view('users', compact('users'));
     }
