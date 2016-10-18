626b29e7f067a77f7ba6d8f4e52b993ad7eb8a9c
Bike Del <home@Homes-iMac.Home>
2016-10-18 11:57:39 +0200

    prevent hello input

--- resources/views/manage-properties.blade.php
+++ resources/views/manage-properties.blade.php
@@ -26,6 +26,11 @@
     background-color: lightblue;
     color:white;
 }
+
+input:-webkit-autofill {
+    -webkit-box-shadow: 0 0 0px 1000px #ffffff inset!important;
+}
+
     table
     {
       table-layout:fixed;
